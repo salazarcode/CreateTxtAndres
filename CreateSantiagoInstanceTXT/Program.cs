f5e3f10fa1965bac22ae3c29d3f2c4d7d785a544
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-01-18 17:41:00 -0400

    My first project with C#

---
+++ CreateSantiagoInstanceTXT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string path = $"{desktopPath}\\Santiago.txt";
+        
+        if (!File.Exists(path))
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Hola Mundo!");
+
+                Console.WriteLine("The file was created successfully.");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("The file already exist.");
+        }
+    }
+}
